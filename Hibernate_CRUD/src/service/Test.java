101e09cc7c6935a0c41ff4e9ab3fdcce47927fb1
SarathchandranPM <112255602+SarathchandranPM@users.noreply.github.com>
2022-11-25 21:54:08 +0530

    Add files via upload

---
+++ Hibernate_CRUD/src/service/Test.java
@@ -0,0 +1,14 @@
+package service;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test {
+	public static void main(String[] args) {
+		System.out.println("connecting to database");
+		SessionFactory factory= new Configuration ()
+				.buildSessionFactory();
+		System.out.println("created");
+	}
+
+}
